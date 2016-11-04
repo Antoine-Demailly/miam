5988c60d285f99cc51da55537595e64b4e203311
JuniorBoaventura <junior.boaventura1@gmail.com>
2016-11-04 12:04:49 +0100

    Get places controller

--- app/components/places/places.controller.js
+++ app/components/places/places.controller.js
@@ -1,15 +1,19 @@
 'use strict';
 
-const http   = require('http');
+const request = require('request');
+const _       = require('lodash');
 
-const APIKey = 'AIzaSyC-fD1eyEoTju7sSVy0hMCSeU4jeJIJ0CY';
-const Host   = 'https://maps.googleapis.com/';
+const APIKey  = 'AIzaSyC-fD1eyEoTju7sSVy0hMCSeU4jeJIJ0CY';
+const Host    = 'https://maps.googleapis.com/';
 
 function PlacesController() {
   let self = this;
 
-  self.location = '48.866096,2.373295';
-  self.parameters = '?location=' + self.location + '&radius=500&type=restaurant';
+  self.location   = '48.866096,2.373295';
+
+  self.parameters = '?location=' +  self.location;
+  self.parameters += '&radius=500&type=restaurant';
+  self.parameters += '&key=' + APIKey;
 
 
   function init() {
@@ -31,10 +35,20 @@ function PlacesController() {
   self.patchPlaces = patchPlaces;
 
   function getPlaces(req, res) {
-    console.log('hello');
-    http.get(Host + '/maps/api/place/nearbysearch/json' + self.parameters, function(res) {
-      console.log(res.body);
-    })
+
+    let url = Host + '/maps/api/place/nearbysearch/json' + self.parameters;
+    console.log(url);
+    request.get(url, function(error, response, body) {
+
+      if (!error) {
+        let places = JSON.parse(body);
+
+        _.forEach(places.results, function(place) {
+          console.log(place.name);
+        });
+
+      }
+    });
 
   }
 
