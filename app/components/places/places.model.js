3948607063a110447b376732ca2a452f14fbc489
JuniorBoaventura <junior.boaventura1@gmail.com>
2016-11-07 16:48:05 +0100

    Update API places YELP

--- app/components/places/places.model.js
+++ app/components/places/places.model.js
@@ -1,6 +1,7 @@
 'use_strict';
 
 const request     = require('request');
+const unirest     = require('unirest');
 const _           = require('lodash');
 
 const APIKey = 'AIzaSyC-fD1eyEoTju7sSVy0hMCSeU4jeJIJ0CY';
@@ -8,17 +9,84 @@ const APIKey = 'AIzaSyC-fD1eyEoTju7sSVy0hMCSeU4jeJIJ0CY';
 function PlacesModel() {
   let self  = this;
   self.Host = 'https://maps.googleapis.com/';
+  self.yelpAPIKey = null;
+
+  function init() {
+    yelpOauth2().then(function(token) {
+      fetchRestaurants(48.866096, 2.373295);
+    });
+
+  }
+
+  init();
+
+  /// Private Methods
+  function yelpOauth2() {
+    var body =  {
+      grant_type:    'client_credentials',
+      client_id:     'cJbX-C_kjpnQ8FoRbbfUgQ',
+      client_secret: 'LAmyZ33FiKz2i4j3tV0kxtTiCcKAw5I00HtqUwWaDBhnUfF22tOA4ljspFE8rzNB'
+    };
+
+    return new Promise(function(resolve, reject) {
+      unirest.post('https://api.yelp.com/oauth2/token')
+      .send(body)
+      .end(function(response) {
+        self.yelpAPIKey = response.body.access_token;
+        resolve(self.yelpAPIKey);
+      });
+    });
+
+  }
 
   /// Public Methods
-  self.fetchRestaurants = fetchRestaurants;
+  self.fetchGoogleRestaurants = fetchGoogleRestaurants;
 
-  function fetchRestaurants(latitude, longitude) {
+  function fetchRestaurants(latitude, longitude, categories = 'restaurants', radius = 500) {
+    let parameters = {latitude: latitude, longitude: longitude, radius: radius,categories: categories};
 
-    self.parameters = '?location=' +  [latitude,longitude].join(',');
-    self.parameters += '&radius=500&type=restaurant';
-    self.parameters += '&key=' + APIKey;
+    return new Promise(function(resolve, reject) {
+      let restaurants = [];
 
-    let url = self.Host + '/maps/api/place/nearbysearch/json' + self.parameters;
+      unirest.get('https://api.yelp.com/v3/businesses/search')
+      .headers({'Authorization': 'Bearer ' + self.yelpAPIKey})
+      .query(parameters)
+      .end(function(response) {
+
+        _.forEach(response.body.businesses, function(place) {
+          restaurants.push({
+            title:     place.name,
+            image_url: place.image_url,
+            subtitle:  formatDescription(place.rating, place.price)
+          });
+        });
+
+        restaurants = _.shuffle(restaurants);
+        restaurants = _.chunk(restaurants, 10)[0];
+        resolve(restaurants);
+        console.log(restaurants);
+      });
+
+    });
+  }
+
+  function formatDescription(rating, price) {
+    let desc = '';
+    let rate = Math.ceil(rating);
+    desc    += Array(rate).fill('&#9733;').join('');
+    desc    += Array(5 - rate).fill('&#9734;').join('');
+    desc    += '\n';
+    desc    += price;
+    return desc;
+  }
+
+  function fetchGoogleRestaurants(latitude, longitude) {
+
+    let parameters = '?location=' +  [latitude,longitude].join(',');
+    parameters += '&radius=500&type=restaurant';
+    parameters += '&key=' + APIKey;
+
+    let url = self.Host + '/maps/api/place/nearbysearch/json' + parameters;
     return new Promise(function(resolve, reject) {
 
       // Request the Google API
